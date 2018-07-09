32ef385491f909a2928a7d652b43e969da5e0c3a
unknown <vmirella_17@hotmail.com>
2018-07-09 12:27:54 -0500

    Probando login

---
+++ src/js/main.js
@@ -0,0 +1,34 @@
+// Initialize Firebase
+var config = {
+	apiKey: "AIzaSyD25eue2yTbXemiuIxpsgNsbTq3XD4YR1Y",
+	authDomain: "api-project-235325722491.firebaseapp.com",
+	databaseURL: "https://api-project-235325722491.firebaseio.com",
+	projectId: "api-project-235325722491",
+	storageBucket: "api-project-235325722491.appspot.com",
+	messagingSenderId: "235325722491"
+};
+firebase.initializeApp(config);
+
+const email = document.getElementById('email');
+const pass = document.getElementById('pass');
+const buttonLogin = document.getElementById('login');
+
+buttonLogin.addEventListener('click', () => {
+
+	firebase.auth().signInWithEmailAndPassword(email.value, pass.value)
+		.then((result) => {
+			alert('Bienvenido');
+			//redireccionar a la pagina de inicio
+		})
+		.catch((error) => {
+			let errorCode = error.code;
+			let errorMessage = error.message;
+			if (errorCode === 'auth/wrong-password') {
+				alert('Contraseña incorrecta.');
+			} 
+			else {
+				alert('Usuario o contraseña incorrecto');
+			}
+		});
+	
+});
