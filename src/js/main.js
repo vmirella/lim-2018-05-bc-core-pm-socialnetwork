e82dffe5e2b92ed98d5ac5f860765827afa10d28
GabyMarapi <gaby1017x@gmail.com>
2018-07-11 22:12:28 -0500

    Resolver conflicto para bajar cambio de gh-pages

--- src/js/main.js
+++ src/js/main.js
@@ -1,18 +1,32 @@
 // Initialize Firebase
-var config = {
-    apiKey: "AIzaSyBC6hYyncHRa4zRqpV1xHLE9uH_FIobzjg",
-    authDomain: "socialnetwork-d9247.firebaseapp.com",
-    databaseURL: "https://socialnetwork-d9247.firebaseio.com",
-    projectId: "socialnetwork-d9247",
-    storageBucket: "socialnetwork-d9247.appspot.com",
-    messagingSenderId: "114124006078"
-  };
-  firebase.initializeApp(config);
+let config = {
+	apiKey: "AIzaSyBC6hYyncHRa4zRqpV1xHLE9uH_FIobzjg",
+	authDomain: "socialnetwork-d9247.firebaseapp.com",
+	databaseURL: "https://socialnetwork-d9247.firebaseio.com",
+	projectId: "socialnetwork-d9247",
+	storageBucket: "socialnetwork-d9247.appspot.com",
+	messagingSenderId: "114124006078"
+};
+firebase.initializeApp(config);
 
 const email = document.getElementById('email');
 const pass = document.getElementById('pass');
 const buttonLogin = document.getElementById('button-login');
 const sigInWithFacebookBtn = document.getElementById('sigInWithFacebook');
+const logOutBtn = document.getElementById('logOut');
+
+
+
+logOutBtn.addEventListener('click',()=>{
+	console.log('Signed Out');
+	firebase.auth().signOut().then(function() {
+		console.log('Signed Out');
+		
+		window.location = 'index.html';
+	  }, function(error) {
+		console.error('Sign Out Error', error);
+	  });
+})
 
 buttonLogin.addEventListener('click', () => {
 
@@ -26,40 +40,48 @@ buttonLogin.addEventListener('click', () => {
 			let errorMessage = error.message;
 			if (errorCode === 'auth/wrong-password') {
 				alert('Contraseña incorrecta.');
-			} 
+			}
 			else {
 				alert('Usuario o contraseña incorrecto');
 			}
 		});
-	
+
 });
 
-var provider = new firebase.auth.FacebookAuthProvider();
+let provider = new firebase.auth.FacebookAuthProvider();
 
 
-sigInWithFacebookBtn.addEventListener('click', function() {
-    firebase.auth().signInWithPopup(provider).then(function(result) {
-	 // This gives you a Facebook Access Token. You can use it to access the Facebook API.
-		var token = result.credential.accessToken;
+sigInWithFacebookBtn.adEventListener('click', function () {
+	firebase.auth().signInWithPopup(provider).then(function (result) {
+		// This gives you a Facebook Access Token. You can use it to access the Facebook API.
+		const token = result.credential.accessToken;
 		// The signed-in user info.
-		var user = result.user;
-		buttonLogin.style.display ='none';
+		const user = result.user;
+		window.location = 'home.html';
 		// ...
-	  }).catch(function(error) {
+	}).catch(function (error) {
 		// Handle Errors here.
-		var errorCode = error.code;
-		var errorMessage = error.message;
+		const errorCode = error.code;
+		const errorMessage = error.message;
 		// The email of the user's account used.
-		var email = error.email;
+		const email = error.email;
 		// The firebase.auth.AuthCredential type that was used.
-		var credential = error.credential;
+		const credential = error.credential;
 		// ...
+	});
+});
+
+logOutBtn.addEventListener('click',()=>{
+	firebase.auth().signOut().then(function() {
+		// Sign-out successful.
+		window.location = 'home.html';
+	  }).catch(function(error) {
+		// An error happened.
 	  });
-	
+});
 
-  });
 
- 
 
-  
-  
+
+
+
